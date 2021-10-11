c426d8a2b35f92e4997a6d3389c62f1b70236bb8
mariyapoly <poly.craft2141@gmail.com>
2021-10-11 12:05:13 +0600

    added burj-al-arab

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAV1wVanUdx7V_q-wZKYtgIS6KmZhrxa2A",
+    authDomain: "burj-al-arab-beecf.firebaseapp.com",
+    projectId: "burj-al-arab-beecf",
+    storageBucket: "burj-al-arab-beecf.appspot.com",
+    messagingSenderId: "62779697160",
+    appId: "1:62779697160:web:67539ff8ada3883f16c9bd"
+};
+
+export default firebaseConfig;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,28 @@
-import React, { useContext } from 'react';
-
+import React from 'react';
+import { useForm } from "react-hook-form";
+import './Login.css';
 
 const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
     return (
-        <div>
-            <h1>This is Login</h1>
+        <div className="container">
+            <div className="form-container">
+                <h3>Sign in</h3>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label htmlFor="email">Email address</label>
+                    <input id="email" {...register("email", { required: true })} />
+                    {errors.email && <span>This field is required</span>}
+                    <input className="submit-btn" type="submit" value="Continue with email" />
+                </form>
+                <div>
+                    <p>or use one of these options</p>
+                </div>
+                <button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="facebbok" aria-hidden="true" focusable="false" role="img"><path d="M22.675 0H1.325C.593 0 0 .593 0 1.325v21.351C0 23.407.593 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116c.73 0 1.323-.593 1.323-1.325V1.325C24 .593 23.407 0 22.675 0z"></path></svg></button>
+                <button>
+                    <svg viewBox="0 0 262 262" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" aria-hidden="true" focusable="false" width="24" height="24" role="img"><path d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027" fill="#4285F4"></path><path d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1" fill="#34A853"></path><path d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782" fill="#FBBC05"></path><path d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251" fill="#EB4335"></path></svg>
+                </button>
+            </div>
         </div>
     );
 };
